d506737f11eb1b3ee6c08307d0e54f5da274adf4
jwrmg <69098500+jwrmg@users.noreply.github.com>
2022-05-17 10:54:18 +1000

    added specialization identifiers

--- include/NetworkIdentifiers.h
+++ include/NetworkIdentifiers.h
@@ -5,11 +5,27 @@ namespace Eclipse
 {
 	namespace Networking
 	{
-		enum class NetworkIdentifiers
+		enum NetworkIdentifiers
 		{
 			EID_MESSAGE = ID_USER_PACKET_ENUM + 1,
+			// use this to signify that there is a special id.
+			// and use the special id for the handler.
+			EID_SPECIALIZATION,
 			// extend from here.
 			EID_USER_PACKET_ENUM,
+		};
+
+		enum SpecialNetworkIdentifier : unsigned short
+		{
+			// WARNING: YOU MUST EXTEND FROM HERE
+			// USAGE:
+			/*
+			 *	enum class yourEnum : unsigned short
+			 *	{
+			 *		yourEnumName = SPEC_USER_PACKET_ENUM;
+			 *	}
+			 */
+			SPEC_USER_PACKET_ENUM,
 		};
 	}
 }
